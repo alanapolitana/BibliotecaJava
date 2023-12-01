aeb578485af8ed29af4bd0f17360bfebcbaade30
Marco <marcovirinni@gmail.com>
2023-12-01 19:53:51 -0300

    agrega y elimina libros

--- src/main/java/biblioteca/Biblioteca.java
+++ src/main/java/biblioteca/Biblioteca.java
@@ -1,15 +1,19 @@
 package biblioteca;
 
+import biblioteca.notificacion.AbstractBiblioteca;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Biblioteca {
+public class Biblioteca extends AbstractBiblioteca {
+
     private List<Usuario> usuarios;
     private List<Libro> libros;
     private List<Alquiler> alquileres;
 
-    public Biblioteca() {
+    public Biblioteca(String nombreBiblioteca) {
+        super(nombreBiblioteca);
         this.usuarios = new ArrayList<>();
         this.libros = new ArrayList<>();
         this.alquileres = new ArrayList<>();
@@ -32,12 +36,12 @@ public class Biblioteca {
 
         for (Usuario usuario : usuarios) {
             if (usuario.getCorreo().equals(correo) && usuario.getContraseña().equals(contraseña)) {
-                System.out.println("Inicio de sesión exitoso. ¡Bienvenido, " + usuario.getNombre() + "!");
+                notificarUsuario("Inicio de sesión exitoso. ¡Bienvenido, " + usuario.getNombre() + "!");
                 return usuario;
             }
         }
 
-        System.out.println("Credenciales incorrectas. Por favor, intente nuevamente.");
+        notificarUsuario("Credenciales incorrectas. Por favor, intente nuevamente.");
         return null;
     }
 
@@ -53,7 +57,7 @@ public class Biblioteca {
             System.out.print("Seleccione una opción: ");
 
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer de entrada
+            scanner.nextLine(); 
 
             switch (opcion) {
                 case 1:
@@ -70,7 +74,6 @@ public class Biblioteca {
             }
         }
 
-     
         menuBiblioteca(usuario);
     }
 
@@ -100,11 +103,11 @@ public class Biblioteca {
             System.out.println("2. Alquilar libro");
             System.out.println("3. Devolver libro");
             System.out.println("4. Ver libros alquilados");
-            System.out.println("5. Salir");
-
+            System.out.println("5. Administrar libros"); 
+            System.out.println("6. Salir");
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer de entrada
+            scanner.nextLine(); 
 
             switch (opcion) {
                 case 1:
@@ -119,10 +122,13 @@ public class Biblioteca {
                 case 4:
                     verLibrosAlquilados(usuario);
                     break;
-                    
                 case 5:
+                    menuAdministrarLibros(usuario); 
+                    break;
+                case 6:
                     System.out.println("Saliendo del sistema. ¡Hasta luego!");
                     System.exit(0);
+               
                 default:
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
             }
@@ -159,29 +165,29 @@ public class Biblioteca {
         System.out.println("Libro no disponible o no encontrado. Por favor, seleccione otro libro.");
     }
 
-public void devolverLibro(Usuario usuario) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("\n--- Devolución de Libro ---");
-    System.out.println("Libros alquilados:");
-    for (Alquiler alquiler : usuario.getLibrosAlquilados()) {
-        String estadoDevolucion = alquiler.isDevuelto() ? "Devuelto" : "No devuelto";
-        System.out.println(alquiler.getLibro().getTitulo() + " - Fecha de devolución: " + alquiler.getFechaDevolucion() +
-                " - Estado: " + estadoDevolucion);
-    }
-    System.out.println("Ingrese el título del libro que desea devolver:");
-    String tituloLibro = scanner.nextLine();
-
-    for (Alquiler alquiler : usuario.getLibrosAlquilados()) {
-        if (alquiler.getLibro().getTitulo().equalsIgnoreCase(tituloLibro) && !alquiler.isDevuelto()) {
-            alquiler.setDevuelto(true);
-            alquiler.getLibro().setDisponible(true);
-            System.out.println("Libro devuelto con éxito.");
-            return;
+    public void devolverLibro(Usuario usuario) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n--- Devolución de Libro ---");
+        System.out.println("Libros alquilados:");
+        for (Alquiler alquiler : usuario.getLibrosAlquilados()) {
+            String estadoDevolucion = alquiler.isDevuelto() ? "Devuelto" : "No devuelto";
+            System.out.println(alquiler.getLibro().getTitulo() + " - Fecha de devolución: " + alquiler.getFechaDevolucion() +
+                    " - Estado: " + estadoDevolucion);
         }
-    }
+        System.out.println("Ingrese el título del libro que desea devolver:");
+        String tituloLibro = scanner.nextLine();
 
-    System.out.println("Libro no encontrado en su lista de libros alquilados o ya ha sido devuelto.");
-}
+        for (Alquiler alquiler : usuario.getLibrosAlquilados()) {
+            if (alquiler.getLibro().getTitulo().equalsIgnoreCase(tituloLibro) && !alquiler.isDevuelto()) {
+                alquiler.setDevuelto(true);
+                alquiler.getLibro().setDisponible(true);
+                System.out.println("Libro devuelto con éxito.");
+                return;
+            }
+        }
+
+        System.out.println("Libro no encontrado en su lista de libros alquilados o ya ha sido devuelto.");
+    }
 
     public void verLibrosAlquilados(Usuario usuario) {
         System.out.println("\n--- Libros Alquilados ---");
@@ -190,67 +196,146 @@ public void devolverLibro(Usuario usuario) {
             System.out.println(alquiler.getLibro().getTitulo() + " - Fecha de devolución: " + alquiler.getFechaDevolucion() + estadoDevuelto);
         }
     }
-    public static void main(String[] args) {
-        Biblioteca biblioteca = new Biblioteca();
 
-        
-        Usuario usuario1 = new Usuario(1, "Lionel Messi", "messi@gmail.com", "messi123", new ArrayList<>());
-        Usuario usuario2 = new Usuario(2, "Diego Maradona", "maradona@gmail.com", "maradona123", new ArrayList<>());
-        Usuario usuario3 = new Usuario(3, "Carlos Tevez", "tevez@gmail.com", "tevez123", new ArrayList<>());
-        Usuario usuario4 = new Usuario(4, "Luis Suárez", "suarez@gmail.com", "suarez123", new ArrayList<>());
-        Usuario usuario5 = new Usuario(5, "Juan Román Riquelme", "riquelme@gmail.com", "riquelme123", new ArrayList<>());
+    public void menuAdministrarLibros(Usuario usuario) {
+        Scanner scanner = new Scanner(System.in);
 
-        
-        Libro libro1 = new Libro("Spider-Man: No Way Home", "Various", "Action", true);
-        Libro libro2 = new Libro("Eternals", "Various", "Adventure", true);
-        Libro libro3 = new Libro("Shang-Chi and the Legend of the Ten Rings", "Various", "Fantasy", true);
-        Libro libro4 = new Libro("Black Widow", "Various", "Action", true);
-        Libro libro5 = new Libro("Doctor Strange in the Multiverse of Madness", "Various", "Adventure", true);
+        while (true) {
+            System.out.println("\n--- Administrar Libros ---");
+            System.out.println("1. Agregar libro");
+            System.out.println("2. Eliminar libro");
+            System.out.println("3. Volver al menú principal");
 
-        
-        Libro libro6 = new Libro("To Kill a Mockingbird", "Harper Lee", "Fiction", true);
-        Libro libro7 = new Libro("1984", "George Orwell", "Dystopian", true);
-        Libro libro8 = new Libro("Pride and Prejudice", "Jane Austen", "Romance", true);
-        Libro libro9 = new Libro("The Great Gatsby", "F. Scott Fitzgerald", "Classic", true);
-        Libro libro10 = new Libro("The Catcher in the Rye", "J.D. Salinger", "Coming-of-age", true);
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); 
 
-        
-        Libro libro11 = new Libro("Don Segundo Sombra", "Ricardo Güiraldes", "Gaucho literature", true);
-        Libro libro12 = new Libro("Martín Fierro", "José Hernández", "Epic poetry", true);
-        Libro libro13 = new Libro("Santa Evita", "Tomás Eloy Martínez", "Historical fiction", true);
-        Libro libro14 = new Libro("Hopscotch", "Julio Cortázar", "Experimental fiction", true);
-        Libro libro15 = new Libro("The Invention of Morel", "Adolfo Bioy Casares", "Science fiction", true);
-
-        
-        Alquiler alquiler1 = new Alquiler(usuario1, libro1);
-        Alquiler alquiler2 = new Alquiler(usuario2, libro6);
-
-       
-        biblioteca.registrarUsuario(usuario1);
-        biblioteca.registrarUsuario(usuario2);
-        biblioteca.registrarUsuario(usuario3);
-        biblioteca.registrarUsuario(usuario4);
-        biblioteca.registrarUsuario(usuario5);
-
-        biblioteca.agregarLibro(libro1);
-        biblioteca.agregarLibro(libro2);
-        biblioteca.agregarLibro(libro3);
-        biblioteca.agregarLibro(libro4);
-        biblioteca.agregarLibro(libro5);
-        biblioteca.agregarLibro(libro6);
-        biblioteca.agregarLibro(libro7);
-        biblioteca.agregarLibro(libro8);
-        biblioteca.agregarLibro(libro9);
-        biblioteca.agregarLibro(libro10);
-        biblioteca.agregarLibro(libro11);
-        biblioteca.agregarLibro(libro12);
-        biblioteca.agregarLibro(libro13);
-        biblioteca.agregarLibro(libro14);
-        biblioteca.agregarLibro(libro15);
-
-        biblioteca.alquileres.add(alquiler1);
-        biblioteca.alquileres.add(alquiler2);
-     
-        biblioteca.menuPrincipal();
+            switch (opcion) {
+                case 1:
+                    agregarLibro(usuario);
+                    break;
+                case 2:
+                    eliminarLibro(usuario);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+            }
+        }
     }
+
+    public void agregarLibro(Usuario usuario) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n--- Agregar Libro ---");
+        System.out.println("Ingrese el título del libro:");
+        String tituloLibro = scanner.nextLine();
+        System.out.println("Ingrese el autor del libro:");
+        String autorLibro = scanner.nextLine();
+        System.out.println("Ingrese el género del libro:");
+        String generoLibro = scanner.nextLine();
+
+        Libro nuevoLibro = new Libro(tituloLibro, autorLibro, generoLibro, true);
+        libros.add(nuevoLibro);
+
+        System.out.println("Libro agregado con éxito al catálogo.");
+    }
+
+    public void eliminarLibro(Usuario usuario) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n--- Eliminar Libro ---");
+        verCatalogoLibros();
+
+        System.out.println("Ingrese el título del libro que desea eliminar:");
+        String tituloLibro = scanner.nextLine();
+
+        for (Libro libro : libros) {
+            if (libro.getTitulo().equalsIgnoreCase(tituloLibro)) {
+                libros.remove(libro);
+                System.out.println("Libro eliminado con éxito del catálogo.");
+                return;
+            }
+        }
+
+        System.out.println("Libro no encontrado en el catálogo.");
+    }
+public static void main(String[] args) {
+    Biblioteca biblioteca = new Biblioteca("Biblioteca Ejemplo");
+
+    Usuario usuario1 = new Usuario(1, "Lionel Messi", "messi@gmail.com", "messi123", new ArrayList<>());
+    Usuario usuario2 = new Usuario(2, "Diego Maradona", "maradona@gmail.com", "maradona123", new ArrayList<>());
+    Usuario usuario3 = new Usuario(3, "Carlos Tevez", "tevez@gmail.com", "tevez123", new ArrayList<>());
+    Usuario usuario4 = new Usuario(4, "Luis Suárez", "suarez@gmail.com", "suarez123", new ArrayList<>());
+    Usuario usuario5 = new Usuario(5, "Juan Román Riquelme", "riquelme@gmail.com", "riquelme123", new ArrayList<>());
+    Usuario usuario6 = new Usuario(6, "Gabriel Batistuta", "batistuta@gmail.com", "batistuta123", new ArrayList<>());
+    Usuario usuario7 = new Usuario(7, "Javier Zanetti", "zanetti@gmail.com", "zanetti123", new ArrayList<>());
+    Usuario usuario8 = new Usuario(8, "Ariel Ortega", "ortega@gmail.com", "ortega123", new ArrayList<>());
+    Usuario usuario9 = new Usuario(9, "Hernan Crespo", "crespo@gmail.com", "crespo123", new ArrayList<>());
+    Usuario usuario10 = new Usuario(10, "Pablo Aimar", "aimar@gmail.com", "aimar123", new ArrayList<>());
+
+    Libro libro1 = new Libro("Spider-Man: No Way Home", "Various", "Action", true);
+    Libro libro2 = new Libro("Eternals", "Various", "Adventure", true);
+    Libro libro3 = new Libro("Shang-Chi and the Legend of the Ten Rings", "Various", "Fantasy", true);
+    Libro libro4 = new Libro("Black Widow", "Various", "Action", true);
+    Libro libro5 = new Libro("Doctor Strange in the Multiverse of Madness", "Various", "Adventure", true);
+    Libro libro6 = new Libro("To Kill a Mockingbird", "Harper Lee", "Fiction", true);
+    Libro libro7 = new Libro("1984", "George Orwell", "Dystopian", true);
+    Libro libro8 = new Libro("Pride and Prejudice", "Jane Austen", "Romance", true);
+    Libro libro9 = new Libro("The Great Gatsby", "F. Scott Fitzgerald", "Classic", true);
+    Libro libro10 = new Libro("The Catcher in the Rye", "J.D. Salinger", "Coming-of-age", true);
+    Libro libro11 = new Libro("Don Segundo Sombra", "Ricardo Güiraldes", "Gaucho literature", true);
+    Libro libro12 = new Libro("Martín Fierro", "José Hernández", "Epic poetry", true);
+    Libro libro13 = new Libro("Santa Evita", "Tomás Eloy Martínez", "Historical fiction", true);
+    Libro libro14 = new Libro("Hopscotch", "Julio Cortázar", "Experimental fiction", true);
+    Libro libro15 = new Libro("The Invention of Morel", "Adolfo Bioy Casares", "Science fiction", true);
+    Libro libro16 = new Libro("The Lord of the Rings", "J.R.R. Tolkien", "Fantasy", true);
+    Libro libro17 = new Libro("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasy", true);
+    Libro libro18 = new Libro("The Da Vinci Code", "Dan Brown", "Mystery", true);
+    Libro libro19 = new Libro("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "Science fiction", true);
+    Libro libro20 = new Libro("The Hunger Games", "Suzanne Collins", "Dystopian", true);
+    Libro libro21 = new Libro("The Shining", "Stephen King", "Horror", true);
+    Libro libro22 = new Libro("Brave New World", "Aldous Huxley", "Science fiction", true);
+    Libro libro23 = new Libro("The Martian", "Andy Weir", "Science fiction", true);
+    Libro libro24 = new Libro("The Great Expectations", "Charles Dickens", "Classic", true);
+    Libro libro25 = new Libro("The Chronicles of Narnia", "C.S. Lewis", "Fantasy", true);
+
+    biblioteca.registrarUsuario(usuario1);
+    biblioteca.registrarUsuario(usuario2);
+    biblioteca.registrarUsuario(usuario3);
+    biblioteca.registrarUsuario(usuario4);
+    biblioteca.registrarUsuario(usuario5);
+    biblioteca.registrarUsuario(usuario6);
+    biblioteca.registrarUsuario(usuario7);
+    biblioteca.registrarUsuario(usuario8);
+    biblioteca.registrarUsuario(usuario9);
+    biblioteca.registrarUsuario(usuario10);
+
+    biblioteca.agregarLibro(libro1);
+    biblioteca.agregarLibro(libro2);
+    biblioteca.agregarLibro(libro3);
+    biblioteca.agregarLibro(libro4);
+    biblioteca.agregarLibro(libro5);
+    biblioteca.agregarLibro(libro6);
+    biblioteca.agregarLibro(libro7);
+    biblioteca.agregarLibro(libro8);
+    biblioteca.agregarLibro(libro9);
+    biblioteca.agregarLibro(libro10);
+    biblioteca.agregarLibro(libro11);
+    biblioteca.agregarLibro(libro12);
+    biblioteca.agregarLibro(libro13);
+    biblioteca.agregarLibro(libro14);
+    biblioteca.agregarLibro(libro15);
+    biblioteca.agregarLibro(libro16);
+    biblioteca.agregarLibro(libro17);
+    biblioteca.agregarLibro(libro18);
+    biblioteca.agregarLibro(libro19);
+    biblioteca.agregarLibro(libro20);
+    biblioteca.agregarLibro(libro21);
+    biblioteca.agregarLibro(libro22);
+    biblioteca.agregarLibro(libro23);
+    biblioteca.agregarLibro(libro24);
+    biblioteca.agregarLibro(libro25);
+
+    biblioteca.menuPrincipal();
+}
 }
--- src/main/java/biblioteca/notificacion/AbstractBiblioteca.java
+++ src/main/java/biblioteca/notificacion/AbstractBiblioteca.java
@@ -1,5 +1,19 @@
 package biblioteca.notificacion;
 
 public abstract class AbstractBiblioteca implements SistemaNotificacion {
-  
+
+    protected String nombreBiblioteca;
+
+    public AbstractBiblioteca(String nombreBiblioteca) {
+        this.nombreBiblioteca = nombreBiblioteca;
+    }
+
+    public String getNombreBiblioteca() {
+        return nombreBiblioteca;
+    }
+
+    @Override
+    public void notificarUsuario(String mensaje) {
+        System.out.println("Notificación de la biblioteca '" + getNombreBiblioteca() + "': " + mensaje);
+    }
 }
